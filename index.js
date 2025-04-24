520c10d1795fb6fa3412b361fea70d4503708385
Alyssa Tandingan <aly.tandingan@gmail.com>
2025-04-23 22:06:52 -0400

    added form validation on inputs, rsvp modal, and an animation on successful input

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/*** Dark Mode ***
+  
+  Purpose:
+  - Use this starter code to add a dark mode feature to your website.
+
+  When To Modify:
+  - [X] Project 5 (REQUIRED FEATURE) 
+  - [ ] Any time after
+***/
 
 // Step 1: Select the theme button
 
@@ -6,13 +15,13 @@ let themeButton = document.getElementById('theme-button');
 // Step 2: Write the callback function
 const toggleLightMode = () => {
   document.body.classList.toggle("light-mode");
-  document.html.classList.toggle("light-mode");
 }
 
 // Step 3: Register a 'click' event listener for the theme button
 //             and tell it to use toggleDarkMode as its callback function
 
 themeButton.addEventListener('click', toggleLightMode);
+
 
 /*** Form Handling ***
   
@@ -30,19 +39,91 @@ themeButton.addEventListener('click', toggleLightMode);
 
 const rsvpButton = document.querySelector("#rsvp-button");
 
-const addParticipant = (event) => {
+const addParticipant = (person) => {
   // Step 2: Write your code to manipulate the DOM here
-  const name = document.querySelector("#name").value;
-  const address = document.querySelector("#address").value;
-  const email = document.querySelector("#email").value;
-
   //appending to DOM
   const participant = document.createElement("p");
-  participant.innerText = `🎟️ ${name} from ${address} has RSVP'd.`;
+  participant.innerText = `🎟️ ${person.name} from ${person.hometown} has RSVP'd.`;
   document.querySelector(".rsvp-participants").appendChild(participant);
-  event.preventDefault();
 }
 
-// Step 3: Add a click event listener to the submit RSVP button here
 
-rsvpButton.addEventListener("click", addParticipant);
+/*** Form Validation ***/
+
+const validateForm = (event) => {
+
+  let containsErrors = false;
+
+  var rsvpInputs = document.getElementById("rsvp-form").elements;
+
+  let person = {
+    name: rsvpInputs[0].value,
+    hometown: rsvpInputs[1].value,
+    email: rsvpInputs[2].value
+  };
+
+  // TODO: Loop through all inputs
+  for (let i = 0; i < rsvpInputs.length; i++) {
+    // TODO: Inside loop, validate the value of each input
+    if (person.hometown.length < 2) {
+      containsErrors = true;
+      rsvpInputs[i].classList.add("error");
+      event.preventDefault();
+    } else {
+      rsvpInputs[i].classList.remove("error");
+    }
+  }
+
+  if (containsErrors === false) {
+    addParticipant(person);
+    toggleModal(person);
+
+    for (let i = 0; i < rsvpInputs.length; i++) {
+      rsvpInputs[i].value = "";
+    }
+  }
+
+}
+
+// Step 3: Replace the form button's event listener with a new one that calls validateForm()
+rsvpButton.addEventListener("click", validateForm);
+
+const toggleModal = (person) => {
+  let modal = document.querySelector("#success-modal");
+  let modalContent = document.querySelector("#modal-text");
+
+  // TODO: Update modal display to flex
+
+  modal.style.display = "flex";
+
+  // TODO: Update modal text to personalized message
+
+  modalContent.innerHTML = `The Mad Hatter gladly accepts your reservation, ${person.name} <br>
+                        <br> ♦♦♦♦♦♦
+                        <br>
+                        <br>
+                        Tea and refreshments will be made posthaste. We look forward to having a very mad time together.</p>`
+
+  // Set modal timeout to 5 seconds
+  setTimeout(() => {
+    modal.style.display = "none";
+    clearInterval(intervalID);
+  }, 4000);
+
+  let intervalID = setInterval(animateImage, 500);
+}
+
+// TODO: animation variables and animateImage() function 
+
+let rotateFactor = 0;
+let modalImage = document.querySelector("#modal-img");
+
+const animateImage = () => {
+  if (rotateFactor === 0) {
+    rotateFactor = -10;
+  } else {
+    rotateFactor = 0;
+  }
+
+  modalImage.style.transform = `rotate(${rotateFactor}deg)`;
+}
